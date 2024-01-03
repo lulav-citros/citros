1555494474b7e4b7731354b4a7e2a53b6d611116
Lidia <120949888+Yalyalieva@users.noreply.github.com>
2024-01-03 09:42:36 +0200

    fix(citros/citros_data_analysis): adjustments and documentation generation (#22)

--- citros/citros_data_analysis/data_access/_stat.py
+++ citros/citros_data_analysis/data_access/_stat.py
@@ -1,7 +1,6 @@
 from .citros_db import CitrosDB
 from .citros_dict import CitrosDict
 
-
 class Stat:
     """
     Class for collecting technical statistics about connections from the CitrosDB class.
@@ -10,9 +9,6 @@ class Stat:
     about how many connections and queries were done by all CitrosDB objects existing in the current session.
     The information is recorded only for those CitrosDB that were created with parameter debug = True:
 
-    - `n_gql_connections` - number of graphql connections
-    - `n_gql_queries` - number of graphql queries
-    - `gql_calls` - the statistics of how many queries to gql database were done by each method.
     - `n_pg_connection` - number of postgres connections
     - `n_pg_queries` - number of postgres queries
     - `pg_calls` - the statistics of how many queries to postgres database were done by each method.
@@ -31,23 +27,15 @@ class Stat:
         -------
         stat: dict or citros_data_analysis.data_access.citros_dict.CitrosDict
         """
-        gql = {
-            "n_gql_connections": CitrosDB.n_gql_connections,
-            "n_gql_queries": CitrosDB.n_gql_queries,
-        }
-        pg = {
+        stat = {
             "n_pg_connections": CitrosDB.n_pg_connections,
             "n_pg_queries": CitrosDB.n_pg_queries,
         }
         if format == "CitrosDict":
-            gql["gql_calls"] = CitrosDict(CitrosDB.gql_calls)
-            pg["pg_calls"] = CitrosDict(CitrosDB.pg_calls)
-            stat = {**gql, **pg}
+            stat["pg_calls"] = CitrosDict(CitrosDB.pg_calls)
             stat = CitrosDict(stat)
         else:
-            gql["gql_calls"] = CitrosDB.gql_calls
-            pg["pg_calls"] = CitrosDB.pg_calls
-            stat = {**gql, **pg}
+            stat["pg_calls"] = CitrosDB.pg_calls
         return stat
 
     def print_stat(self):
